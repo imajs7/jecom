3de4713511270dc3ff29099e7df69b6c751a49df
imajs7 <anukhg@gmail.com>
2022-06-27 17:14:06 +0530

    category order by include demonstrated

--- jsm-engine/create_homepage.php
+++ jsm-engine/create_homepage.php
@@ -57,85 +57,86 @@ function build_homepage_page() {
 
         <tr valign="top">
         <th scope="col">Settings for section 1</th>
-            <td>
+            <td style="border: 1px solid #aaa; padding: 20px">
                 <label>Section Title</label><br/>
-                <input style="width: 600px" type="text" name="section_1_title" value="<?php echo esc_attr( get_option('section_1_title') ); ?>" />
+                <input style="width: 100%" type="text" name="section_1_title" value="<?php echo esc_attr( get_option('section_1_title') ); ?>" />
                 <br/><br/>
                 <label>Section Shortcode</label><br/>
-                <input style="width: 600px" type="text" name="section_1_shortcode" value="<?php echo esc_attr( get_option('section_1_shortcode') ); ?>" />
+                <input style="width: 100%" type="text" name="section_1_shortcode" value="<?php echo esc_attr( get_option('section_1_shortcode') ); ?>" />
                 <br/><br/>
                 <label>Section Description</label><br/>
-                <input style="width: 600px" type="text" name="section_1_description" value="<?php echo esc_attr( get_option('section_1_description') ); ?>" />
+                <input style="width: 100%" type="text" name="section_1_description" value="<?php echo esc_attr( get_option('section_1_description') ); ?>" />
+                </div>
             </td>
         </tr>
 
         <tr valign="top">
         <th scope="col">Settings for section 2</th>
-            <td>
+            <td style="border: 1px solid #aaa; padding: 20px">
                 <label>Section Title</label><br/>
-                <input style="width: 600px" type="text" name="section_2_title" value="<?php echo esc_attr( get_option('section_2_title') ); ?>" />
+                <input style="width: 100%" type="text" name="section_2_title" value="<?php echo esc_attr( get_option('section_2_title') ); ?>" />
                 <br/><br/>
                 <label>Section Shortcode</label><br/>
-                <input style="width: 600px" type="text" name="section_2_shortcode" value="<?php echo esc_attr( get_option('section_2_shortcode') ); ?>" />
+                <input style="width: 100%" type="text" name="section_2_shortcode" value="<?php echo esc_attr( get_option('section_2_shortcode') ); ?>" />
                 <br/><br/>
                 <label>Section Description</label><br/>
-                <input style="width: 600px" type="text" name="section_2_description" value="<?php echo esc_attr( get_option('section_2_description') ); ?>" />
+                <input style="width: 100%" type="text" name="section_2_description" value="<?php echo esc_attr( get_option('section_2_description') ); ?>" />
             </td>
         </tr>
 
         <tr valign="top">
         <th scope="col">Settings for section 3</th>
-            <td>
+            <td style="border: 1px solid #aaa; padding: 20px">
                 <label>Section Title</label><br/>
-                <input style="width: 600px" type="text" name="section_3_title" value="<?php echo esc_attr( get_option('section_3_title') ); ?>" />
+                <input style="width: 100%" type="text" name="section_3_title" value="<?php echo esc_attr( get_option('section_3_title') ); ?>" />
                 <br/><br/>
                 <label>Section Shortcode</label><br/>
-                <input style="width: 600px" type="text" name="section_3_shortcode" value="<?php echo esc_attr( get_option('section_3_shortcode') ); ?>" />
+                <input style="width: 100%" type="text" name="section_3_shortcode" value="<?php echo esc_attr( get_option('section_3_shortcode') ); ?>" />
                 <br/><br/>
                 <label>Section Description</label><br/>
-                <input style="width: 600px" type="text" name="section_3_description" value="<?php echo esc_attr( get_option('section_3_description') ); ?>" />
+                <input style="width: 100%" type="text" name="section_3_description" value="<?php echo esc_attr( get_option('section_3_description') ); ?>" />
             </td>
         </tr>
          
         <tr valign="top">
         <th scope="col">Settings for section 4</th>
-            <td>
+            <td style="border: 1px solid #aaa; padding: 20px">
                 <label>Section Title</label><br/>
-                <input style="width: 600px" type="text" name="section_4_title" value="<?php echo esc_attr( get_option('section_4_title') ); ?>" />
+                <input style="width: 100%" type="text" name="section_4_title" value="<?php echo esc_attr( get_option('section_4_title') ); ?>" />
                 <br/><br/>
                 <label>Section Shortcode</label><br/>
-                <input style="width: 600px" type="text" name="section_4_shortcode" value="<?php echo esc_attr( get_option('section_4_shortcode') ); ?>" />
+                <input style="width: 100%" type="text" name="section_4_shortcode" value="<?php echo esc_attr( get_option('section_4_shortcode') ); ?>" />
                 <br/><br/>
                 <label>Section Description</label><br/>
-                <input style="width: 600px" type="text" name="section_4_description" value="<?php echo esc_attr( get_option('section_4_description') ); ?>" />
+                <input style="width: 100%" type="text" name="section_4_description" value="<?php echo esc_attr( get_option('section_4_description') ); ?>" />
             </td>
         </tr>
 
         <tr valign="top">
         <th scope="col">Settings for section 5</th>
-            <td>
+            <td style="border: 1px solid #aaa; padding: 20px">
                 <label>Section Title</label><br/>
-                <input style="width: 600px" type="text" name="section_5_title" value="<?php echo esc_attr( get_option('section_5_title') ); ?>" />
+                <input style="width: 100%" type="text" name="section_5_title" value="<?php echo esc_attr( get_option('section_5_title') ); ?>" />
                 <br/><br/>
                 <label>Section Shortcode</label><br/>
-                <input style="width: 600px" type="text" name="section_5_shortcode" value="<?php echo esc_attr( get_option('section_5_shortcode') ); ?>" />
+                <input style="width: 100%" type="text" name="section_5_shortcode" value="<?php echo esc_attr( get_option('section_5_shortcode') ); ?>" />
                 <br/><br/>
                 <label>Section Description</label><br/>
-                <input style="width: 600px" type="text" name="section_5_description" value="<?php echo esc_attr( get_option('section_5_description') ); ?>" />
+                <input style="width: 100%" type="text" name="section_5_description" value="<?php echo esc_attr( get_option('section_5_description') ); ?>" />
             </td>
         </tr>
 
         <tr valign="top">
         <th scope="col">Settings for section 6</th>
-            <td>
+            <td style="border: 1px solid #aaa; padding: 20px">
                 <label>Section Title</label><br/>
-                <input style="width: 600px" type="text" name="section_6_title" value="<?php echo esc_attr( get_option('section_6_title') ); ?>" />
+                <input style="width: 100%" type="text" name="section_6_title" value="<?php echo esc_attr( get_option('section_6_title') ); ?>" />
                 <br/><br/>
                 <label>Section Shortcode</label><br/>
-                <input style="width: 600px" type="text" name="section_6_shortcode" value="<?php echo esc_attr( get_option('section_6_shortcode') ); ?>" />
+                <input style="width: 100%" type="text" name="section_6_shortcode" value="<?php echo esc_attr( get_option('section_6_shortcode') ); ?>" />
                 <br/><br/>
                 <label>Section Description</label><br/>
-                <input style="width: 600px" type="text" name="section_6_description" value="<?php echo esc_attr( get_option('section_6_description') ); ?>" />
+                <input style="width: 100%" type="text" name="section_6_description" value="<?php echo esc_attr( get_option('section_6_description') ); ?>" />
             </td>
         </tr>
 
