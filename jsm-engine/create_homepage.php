0f117d0c10e765b9fcb784e3b2ef92658f5cfba2
imajs7 <anukhg@gmail.com>
2022-07-01 02:54:54 +0530

    build home page css

--- jsm-engine/create_homepage.php
+++ jsm-engine/create_homepage.php
@@ -66,140 +66,140 @@ function build_homepage_page() {
     <?php do_settings_sections( 'build-homepage-group' ); ?>
     <table class="form-table">
 
-        <tr valign="top">
-        <th scope="col">Settings for Category Grid</th>
-            <td style="border: 1px solid #aaa; padding: 20px">
+        <tr class="row">
+        <th class="col">Settings for Category Grid</th>
+            <td>
 
                 <input type="checkbox" name="category_section_enable" value="1" <?php checked(1, get_option('category_section_enable'), true); ?> /><label>Show on frontpage</label><br/><br/>
 
                 <label>Section Title</label><br/>
-                <input style="width: 100%" type="text" name="category_section_title" value="<?php echo esc_attr( get_option('category_section_title') ); ?>" />
+                <input type="text" name="category_section_title" value="<?php echo esc_attr( get_option('category_section_title') ); ?>" />
                 <br/><br/>
 
                 <label>IDs to display</label><br/>
-                <input style="width: 100%" type="text" name="category_section_ids" value="<?php echo esc_attr( get_option('category_section_ids') ); ?>" />
+                <input type="text" name="category_section_ids" value="<?php echo esc_attr( get_option('category_section_ids') ); ?>" />
                 <br/>
                 <p>Enter IDs separated by comma in that order</p>
 
             </td>
         </tr>
 
-        <tr valign="top">
-        <th scope="col">Settings for section 1</th>
-            <td style="border: 1px solid #aaa; padding: 20px">
+        <tr class="row">
+        <th class="col">Settings for section 1</th>
+            <td>
 
                 <input type="checkbox" name="section_1_enable" value="1" <?php checked(1, get_option('section_1_enable'), true); ?> /><label>Show on frontpage</label><br/><br/>
                 
                 <label>Section Title</label><br/>
-                <input style="width: 100%" type="text" name="section_1_title" value="<?php echo esc_attr( get_option('section_1_title') ); ?>" />
+                <input type="text" name="section_1_title" value="<?php echo esc_attr( get_option('section_1_title') ); ?>" />
                 <br/><br/>
                 
                 <label>Section Shortcode</label><br/>
-                <input style="width: 100%" type="text" name="section_1_shortcode" value="<?php echo esc_attr( get_option('section_1_shortcode') ); ?>" />
+                <input type="text" name="section_1_shortcode" value="<?php echo esc_attr( get_option('section_1_shortcode') ); ?>" />
                 <br/><br/>
                 
                 <label>Section Description</label><br/>
-                <input style="width: 100%" type="text" name="section_1_description" value="<?php echo esc_attr( get_option('section_1_description') ); ?>" />
+                <input type="text" name="section_1_description" value="<?php echo esc_attr( get_option('section_1_description') ); ?>" />
                 
             </td>
         </tr>
 
-        <tr valign="top">
-        <th scope="col">Settings for section 2</th>
-            <td style="border: 1px solid #aaa; padding: 20px">
+        <tr class="row">
+        <th class="col">Settings for section 2</th>
+            <td>
 
                 <input type="checkbox" name="section_2_enable" value="1" <?php checked(1, get_option('section_2_enable'), true); ?> /><label>Show on frontpage</label><br/><br/>
 
                 <label>Section Title</label><br/>
-                <input style="width: 100%" type="text" name="section_2_title" value="<?php echo esc_attr( get_option('section_2_title') ); ?>" />
+                <input type="text" name="section_2_title" value="<?php echo esc_attr( get_option('section_2_title') ); ?>" />
                 <br/><br/>
 
                 <label>Section Shortcode</label><br/>
-                <input style="width: 100%" type="text" name="section_2_shortcode" value="<?php echo esc_attr( get_option('section_2_shortcode') ); ?>" />
+                <input type="text" name="section_2_shortcode" value="<?php echo esc_attr( get_option('section_2_shortcode') ); ?>" />
                 <br/><br/>
 
                 <label>Section Description</label><br/>
-                <input style="width: 100%" type="text" name="section_2_description" value="<?php echo esc_attr( get_option('section_2_description') ); ?>" />
+                <input type="text" name="section_2_description" value="<?php echo esc_attr( get_option('section_2_description') ); ?>" />
             
             </td>
         </tr>
 
-        <tr valign="top">
-        <th scope="col">Settings for section 3</th>
-            <td style="border: 1px solid #aaa; padding: 20px">
+        <tr class="row">
+        <th class="col">Settings for section 3</th>
+            <td>
 
                 <input type="checkbox" name="section_3_enable" value="1" <?php checked(1, get_option('section_3_enable'), true); ?> /><label>Show on frontpage</label><br/><br/>
                 
                 <label>Section Title</label><br/>
-                <input style="width: 100%" type="text" name="section_3_title" value="<?php echo esc_attr( get_option('section_3_title') ); ?>" />
+                <input type="text" name="section_3_title" value="<?php echo esc_attr( get_option('section_3_title') ); ?>" />
                 <br/><br/>
                 
                 <label>Section Shortcode</label><br/>
-                <input style="width: 100%" type="text" name="section_3_shortcode" value="<?php echo esc_attr( get_option('section_3_shortcode') ); ?>" />
+                <input type="text" name="section_3_shortcode" value="<?php echo esc_attr( get_option('section_3_shortcode') ); ?>" />
                 <br/><br/>
                 
                 <label>Section Description</label><br/>
-                <input style="width: 100%" type="text" name="section_3_description" value="<?php echo esc_attr( get_option('section_3_description') ); ?>" />
+                <input type="text" name="section_3_description" value="<?php echo esc_attr( get_option('section_3_description') ); ?>" />
             
             </td>
         </tr>
          
-        <tr valign="top">
-        <th scope="col">Settings for section 4</th>
-            <td style="border: 1px solid #aaa; padding: 20px">
+        <tr class="row">
+        <th class="col">Settings for section 4</th>
+            <td>
 
                 <input type="checkbox" name="section_4_enable" value="1" <?php checked(1, get_option('section_4_enable'), true); ?> /><label>Show on frontpage</label><br/><br/>
                 
                 <label>Section Title</label><br/>
-                <input style="width: 100%" type="text" name="section_4_title" value="<?php echo esc_attr( get_option('section_4_title') ); ?>" />
+                <input type="text" name="section_4_title" value="<?php echo esc_attr( get_option('section_4_title') ); ?>" />
                 <br/><br/>
                 
                 <label>Section Shortcode</label><br/>
-                <input style="width: 100%" type="text" name="section_4_shortcode" value="<?php echo esc_attr( get_option('section_4_shortcode') ); ?>" />
+                <input type="text" name="section_4_shortcode" value="<?php echo esc_attr( get_option('section_4_shortcode') ); ?>" />
                 <br/><br/>
                 
                 <label>Section Description</label><br/>
-                <input style="width: 100%" type="text" name="section_4_description" value="<?php echo esc_attr( get_option('section_4_description') ); ?>" />
+                <input type="text" name="section_4_description" value="<?php echo esc_attr( get_option('section_4_description') ); ?>" />
             
             </td>
         </tr>
 
-        <tr valign="top">
-        <th scope="col">Settings for section 5</th>
-            <td style="border: 1px solid #aaa; padding: 20px">
+        <tr class="row">
+        <th class="col">Settings for section 5</th>
+            <td>
 
                 <input type="checkbox" name="section_5_enable" value="1" <?php checked(1, get_option('section_5_enable'), true); ?> /><label>Show on frontpage</label><br/><br/>
                 
                 <label>Section Title</label><br/>
-                <input style="width: 100%" type="text" name="section_5_title" value="<?php echo esc_attr( get_option('section_5_title') ); ?>" />
+                <input type="text" name="section_5_title" value="<?php echo esc_attr( get_option('section_5_title') ); ?>" />
                 <br/><br/>
                 
                 <label>Section Shortcode</label><br/>
-                <input style="width: 100%" type="text" name="section_5_shortcode" value="<?php echo esc_attr( get_option('section_5_shortcode') ); ?>" />
+                <input type="text" name="section_5_shortcode" value="<?php echo esc_attr( get_option('section_5_shortcode') ); ?>" />
                 <br/><br/>
                 
                 <label>Section Description</label><br/>
-                <input style="width: 100%" type="text" name="section_5_description" value="<?php echo esc_attr( get_option('section_5_description') ); ?>" />
+                <inp type="text" name="section_5_description" value="<?php echo esc_attr( get_option('section_5_description') ); ?>" />
             
             </td>
         </tr>
 
-        <tr valign="top">
-        <th scope="col">Settings for section 6</th>
-            <td style="border: 1px solid #aaa; padding: 20px">
+        <tr class="row">
+        <th class="col">Settings for section 6</th>
+            <td>
 
                 <input type="checkbox" name="section_6_enable" value="1" <?php checked(1, get_option('section_6_enable'), true); ?> /><label>Show on frontpage</label><br/><br/>
                 
                 <label>Section Title</label><br/>
-                <input style="width: 100%" type="text" name="section_6_title" value="<?php echo esc_attr( get_option('section_6_title') ); ?>" />
+                <input type="text" name="section_6_title" value="<?php echo esc_attr( get_option('section_6_title') ); ?>" />
                 <br/><br/>
                 
                 <label>Section Shortcode</label><br/>
-                <input style="width: 100%" type="text" name="section_6_shortcode" value="<?php echo esc_attr( get_option('section_6_shortcode') ); ?>" />
+                <input type="text" name="section_6_shortcode" value="<?php echo esc_attr( get_option('section_6_shortcode') ); ?>" />
                 <br/><br/>
                 
                 <label>Section Description</label><br/>
-                <input style="width: 100%" type="text" name="section_6_description" value="<?php echo esc_attr( get_option('section_6_description') ); ?>" />
+                <input type="text" name="section_6_description" value="<?php echo esc_attr( get_option('section_6_description') ); ?>" />
             
             </td>
         </tr>
@@ -210,5 +210,13 @@ function build_homepage_page() {
 
 </form>
 </div>
+<style>
+    .row{
+        margin: 5px;
+        padding: 20px;
+        border: 1px solid #999;
+        border-radius: 10px;
+    }
+</style>
 <?php 
 }
