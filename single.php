f637c1c7cfd53bd63964795081fe9a1b4c776f22
imajs7 <anukhg@gmail.com>
2022-07-08 23:12:58 +0530

    single.php

--- single.php
+++ single.php
@@ -12,29 +12,46 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<div class="container">
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			<div class="row">
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'jcom' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'jcom' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+				<div class="col-12 col-md-8 p-4">
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+					<?php
+						while ( have_posts() ) :
+							the_post();
 
-		endwhile; // End of the loop.
-		?>
+							get_template_part( 'template-parts/content', get_post_type() );
+
+							the_post_navigation(
+								array(
+									'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'jcom' ) . '</span> <span class="nav-title">%title</span>',
+									'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'jcom' ) . '</span> <span class="nav-title">%title</span>',
+								)
+							);
+
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+
+						endwhile; // End of the loop.
+					?>
+
+				</div>
+
+				<div class="col-12 col-md-4 p-4">
+
+					<?php get_sidebar(); ?>
+
+				</div>
+
+			</div>
+
+		</div>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
